55d662dfadd32cef8d24d42cff6acf600715e39f
Sakol Assawasagool <koobitor@gmail.com>
2018-07-21 16:22:32 +0700

    complete realtime database

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,23 @@ class Index extends React.Component {
 
   static async getInitialProps ({req, query}) {
     const user = req && req.seesion ? req.session.decodedToken:null
-    return { user }
+    const item = user && await req.firebaseServer.database().ref('todo').once('value')
+    const todo = item && item.val()
+    return { user, todo }
   }
 
   constructor (props) {
     super(props)
 
     this.state = {
-      user: this.props.user
+      user: this.props.user,
+      todo: this.props.todo,
+      value: ''
     }
+
+    this.databaseInit = this.databaseInit.bind(this)
+    this.addTodo = this.addTodo.bind(this)
+    this.send = this.send.bind(this)
   }
 
   componentDidMount () {
@@ -22,26 +30,50 @@ class Index extends React.Component {
       firebase.initializeApp(clientCredentials)
     }
 
+    if(this.state.user) this.databaseInit()
+
     firebase.auth().onAuthStateChanged(user => {
       if(user){
         this.setState({ user: user })
-        user.getIdToken()
+        return user.getIdToken()
           .then((token) => {
-            fetch('/api/login',{
-              method: 'POST',
-              headers: new Headers({ 'Content-Type': 'application/json' }),
-              credentials: 'same-origin',
-              body: JSON.stringify({ token })
-            })
+            // return fetch('/api/login', {
+            //   method: 'POST',
+            //   headers: new Headers({ 'Content-Type': 'application/json' }),
+            //   credentials: 'same-origin',
+            //   body: JSON.stringify({ token })
+            // })
           })
+          .then((res) => this.databaseInit())
       }else{
         this.setState({ user: null })
-        fetch('/api/logout',{
+        fetch('/api/logout', {
           method: 'POST',
           credentials: 'same-origin'
-        })
+        }).then(() => firebase.database().ref('messages').off())
       }
     })
+  }
+
+  databaseInit() {
+    firebase.database().ref('todo').on('value', item => {
+      const todo = item.val()
+      if(todo) this.setState({todo})
+    })
+  }
+
+  addTodo(e) {
+    this.setState({ value: e.target.value })
+  }
+
+  send(e) {
+    e.preventDefault()
+    const date = new Date().getTime()
+    firebase.database().ref(`todo/${date}`).set({
+      id: date,
+      title: this.state.value
+    })
+    this.setState({ value: '' })
   }
 
   login() {
@@ -53,13 +85,32 @@ class Index extends React.Component {
   }
 
   render() {
-    const { user } = this.state
+    const { user, todo, value } = this.state
     return (
       <div>
         {
           user
             ? <button onClick={this.logout}>Logout</button>
             : <button onClick={this.login}>Login</button>
+        }
+        {
+          user &&
+          <div>
+            <form onSubmit={this.send}>
+              <input
+                type="text"
+                onChange={this.addTodo}
+                placeholder="Add your task."
+                value={value} />
+            </form>
+            <ul>
+              {
+                todo && Object.keys(todo).map((key) => (
+                  <li key={key}>{todo[key].title}</li>
+                ))
+            }
+            </ul>
+          </div>
         }
       </div>
     )
