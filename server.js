c04d31f75c4b8986239880b95103345f20214d07
Sakol Assawasagool <koobitor@gmail.com>
2018-07-21 15:02:24 +0700

    init express server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = express()
+
+    server.use(bodyParser.json())
+    server.use(session({
+      secret: 'geheimnis',
+      saveUninitialized: true,
+      store: new FileStore({path: '/tmp/sessions', secret: 'geheimnis'}),
+      resave: false,
+      rolling: true,
+      httpOnly: true,
+      cookie: { maxAge: 604800000 } // week
+    }))
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
