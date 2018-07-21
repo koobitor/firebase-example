9ef353c462362e69e83ed9909001e2b05c9fa3ae
Sakol Assawasagool <koobitor@gmail.com>
2018-07-21 15:38:47 +0700

    login logout

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,53 @@
+import firebase from 'firebase'
 import 'isomorphic-unfetch'
+import clientCredentials from '../credentials/client'
 
 class Index extends React.Component {
 
+  static async getInitialProps ({req, query}) {
+    const user = req && req.seesion ? req.session.decodedToken:null
+    return { user }
+  }
+
   constructor (props) {
     super(props)
+
+    this.state = {
+      user: this.props.user
+    }
   }
 
   componentDidMount () {
+    if(!firebase.apps.length){
+      firebase.initializeApp(clientCredentials)
+    }
 
+    firebase.auth().onAuthStateChanged(user => {
+      if(user){
+        this.setState({ user: user })
+      }else{
+        this.setState({ user: null })
+      }
+    })
+  }
+
+  login() {
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+  }
+
+  logout() {
+    firebase.auth().signOut()
   }
 
   render() {
+    const { user } = this.state
     return (
       <div>
-        Hi
+        {
+          user
+            ? <button onClick={this.logout}>Logout</button>
+            : <button onClick={this.login}>Login</button>
+        }
       </div>
     )
   }
